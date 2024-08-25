2e140fd455953d0062514a0f361371e2897b4ad3
anys34 <anyeseong34@gmail.com>
2024-08-26 04:00:18 +0900

    asdf

---
+++ src/main/java/com/anys34/fiveselves/FiveSelvesApplication.java
@@ -0,0 +1,13 @@
+package com.anys34.fiveselves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveSelvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveSelvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anys34/fiveselves/FiveSelvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anys34.fiveselves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveSelvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
