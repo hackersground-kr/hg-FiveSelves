404021c71958be1e13f1bb5cd3acca7f8b0f3dc7
추성우 <cntjddn715@gmail.com>
2024-08-26 10:33:27 +0900

    색상

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,13 @@
+/*eslint-disable*/
+
 import React from 'react';
+import * as _ from './style';
 
 const Main = () => {
-  return <div>Test</div>;
+  return (
+    <_.Main_Layout>
+    </_.Main_Layout>
+  );
 };
 
 export default Main;
---
+++ src/pages/Main/style.ts
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+import styled from 'styled-components';
+
+export const Main_Layout = styled.div`
+  padding: 0 20px;
+`;
+
+export const Main_SearchBar = styled.div`
+    
+`;
