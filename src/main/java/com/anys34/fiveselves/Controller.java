e19e2b58fa9746c02e4e68b8fca9fbfe9bb21af3
anys34 <anyeseong34@gmail.com>
2024-08-26 15:33:59 +0900

    asdf

--- src/main/java/com/anys34/fiveselves/Controller.java
+++ src/main/java/com/anys34/fiveselves/Controller.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String hello() {
-        return "Hello World!1111";
+        return "Hello World!222";
     }
     @GetMapping("/hello")
     public String hello2() {
-        return "Hello World!1111";
+        return "Hello World!2222";
     }
 }
